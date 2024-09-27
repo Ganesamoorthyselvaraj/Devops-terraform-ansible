d0ee3f11716747c1dc0aa072efd54b60c9eb0179
Ganesamoorthyselvaraj <s.ganesamoorthy@gmail.com>
2024-09-27 14:43:16 +0530

    Update security.tf

--- security.tf
+++ security.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
+  name        = "gaqnesh_allow_ssh"
   description = "Allow SSH inbound traffic"
   vpc_id      = "vpc-00dae5f3df962676d"
 

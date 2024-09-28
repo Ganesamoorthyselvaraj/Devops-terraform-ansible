84b1c25d028624206192dd8bb97bd9fef2e01094
Ganesamoorthyselvaraj <s.ganesamoorthy@gmail.com>
2024-09-28 20:06:02 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -14,7 +14,7 @@ region = "us-east-1"
 data "aws_security_group" "existing_sg" {
   filter {
     name   = "group-name"
-    values = [aws_security_group.allow_ssh.id]
+    values = ["Ganesh-wizard-380"]
   }
 }
 variable "elb-names" {

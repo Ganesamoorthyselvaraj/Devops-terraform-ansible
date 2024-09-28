95538b341ab4445c16f91ea390a278b2531bc0cd
Ganesamoorthyselvaraj <s.ganesamoorthy@gmail.com>
2024-09-28 21:09:14 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -44,8 +44,7 @@ resource "aws_security_group" "allow_ssh" {
 resource "aws_network_interface" "example" {
   count           = 3
   subnet_id       = "subnet-0359cfab31b2e208d"
-  private_ips     = ["172.31.120.11", "172.31.120.12", "172.31.120.13"]
-  security_groups = [aws_security_group.allow_ssh.id]
+    security_groups = [aws_security_group.allow_ssh.id]
 }
 
 resource "aws_instance" "myawsserver" {

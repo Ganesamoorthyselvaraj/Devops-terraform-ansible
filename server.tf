27013e8c246d74cfb612f4daea016b7f555f05fa
Ganesamoorthyselvaraj <s.ganesamoorthy@gmail.com>
2024-09-28 17:03:37 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -14,16 +14,31 @@ region = "us-east-1"
 data "aws_security_group" "existing_sg" {
   name = "gaqnesh_allow_ssh"
 }
-resource "aws_instance" "myawsserver" {
-  ami = "ami-0e54eba7c51c234f6"
-  vpc_security_group_ids = [data.aws_security_group.existing_sg.id]
-  instance_type = "t2.micro"
-  key_name = "ganesh-import"
 
+variable "elb-names" {
+  type = list
+  default = ["ganesh-Kub-Master", "ganesh-Kub-node1","ganesh-Kub-node2"]
+}
+
+variable "list" {
+  type = list
+  default = ["t2.medium","t2.medium","t2.medium"]
+}
+resource "aws_instance" "myawsserver" {
+count =3  
+ami = "ami-0e54eba7c51c234f6"
+  vpc_security_group_ids = [data.aws_security_group.existing_sg.id]
+  instance_type = var.list[count.index]
+  key_name = "ganesh-import"
+network_interface {
+    network_interface_id = aws_network_interface.example[count.index].id
+    device_index         = 0
+}
   tags = {
-    Name = "ganesh-DevOps-batch-server"
+    Name = var.elb-names[count.index]
     env = "Production"
     owner = "Ganesh"
+
   }
   provisioner "local-exec" {
     command = "echo The servers IP address is ${self.public_ip} && echo ${self.public_ip} > /tmp/inv"

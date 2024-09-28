67da687bc38539ef75b5e04ed5eb9700d6a5705b
Ganesamoorthyselvaraj <s.ganesamoorthy@gmail.com>
2024-09-28 17:44:43 +0530

    Update serverip.tf

--- serverip.tf
+++ serverip.tf
@@ -2,5 +2,5 @@ resource "aws_network_interface" "example" {
   count          = 3
   subnet_id      = "subnet-0359cfab31b2e208d"
   private_ips    = ["172.31.120.11", "172.31.120.12","172.31.120.13"]
-  security_groups = ["gaqnesh_allow_ssh"]
+  security_groups = ["Ganesh-wizard-380"]
 }

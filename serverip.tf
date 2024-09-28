bd0a276112d847990f94be5516d39152bac1b394
Ganesamoorthyselvaraj <s.ganesamoorthy@gmail.com>
2024-09-28 20:50:32 +0530

    Delete serverip.tf

--- serverip.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_network_interface" "example" {
-  count          = 3
-  subnet_id      = "subnet-0359cfab31b2e208d"
-  private_ips    = ["172.31.120.220", "172.31.120.221","172.31.120.222"]
-  security_groups = [aws_security_group.allow_ssh.id]
-}

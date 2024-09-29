7405c536e0cfbe0b19f5f56706f861e59d40d3b6
Ganesamoorthyselvaraj <s.ganesamoorthy@gmail.com>
2024-09-29 09:51:29 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -11,58 +11,26 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_security_group" "allow_ssh" {
-  name        = "Ganesh-wizard-380"
-  description = "Allow SSH and HTTP inbound traffic"
-  vpc_id      = "vpc-00dae5f3df962676d"
-
-  ingress {
-    description = "SSH into VPC"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "HTTP into VPC"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    description = "Outbound Allowed"
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+variable "elb-names" {
+  type = list
+  default = ["ganesh-Kub-Master", "ganesh-Kub-node1","ganesh-Kub-node2"]
 }
 
-resource "aws_network_interface" "example" {
-  count           = 3
-  subnet_id       = "subnet-0359cfab31b2e208d"
-    security_groups = [aws_security_group.allow_ssh.id]
+variable "list" {
+  type = list
+  default = ["t2.medium","t2.medium","t2.medium"]
 }
 
-resource "aws_instance" "myawsserver" {
-  count          = 3
-  ami            = "ami-0e54eba7c51c234f6"
-  instance_type  = "t2.medium"
-  key_name       = "ganesh-import"
 
-  network_interface {
-    network_interface_id = aws_network_interface.example[count.index].id
-    device_index         = 0
-  }
-
-  tags = {
-    Name  = "ganesh-Kub-${count.index + 1}"
-    env   = "Production"
-    owner = "Ganesh"
-  }
+resource "aws_instance" "team42" {
+ami = "ami-0522ab6e1ddcc7055"
+key_name = "ganesh-import"
+count= 3
+   instance_type = var.list[count.index]
+tags= {
+Name= var.elb-names[count.index]
+}
+}
 
   provisioner "local-exec" {
     command = <<EOT

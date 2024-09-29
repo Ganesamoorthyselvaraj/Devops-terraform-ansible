c535a19e7a24dd80048a6b7dd67b4ee3610a3a56
Ganesamoorthyselvaraj <s.ganesamoorthy@gmail.com>
2024-09-29 10:02:11 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -1,19 +1,6 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.64.0"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-east-1"
-}
-
 variable "elb-names" {
   type = list
-  default = ["ganesh-Kub-Master", "ganesh-Kub-node1","ganesh-Kub-node2"]
+  default = ["ganesh-Kub-Master1", "ganesh-Kub-node1","ganesh-Kub-node2"]
 }
 
 variable "list" {
@@ -24,7 +11,7 @@ variable "list" {
 
 resource "aws_instance" "team42" {
 ami = "ami-0522ab6e1ddcc7055"
-key_name = "ganesh-import"
+key_name = "team4india"
 count= 3
    instance_type = var.list[count.index]
 tags= {
@@ -35,10 +22,10 @@ Name= var.elb-names[count.index]
   provisioner "local-exec" {
     command = <<EOT
       echo "[master]" > inventory
-      echo "${aws_instance.myawsserver[0].private_ip} ansible_user=ec2-user ansible_ssh_private_key_file=~/.ssh/ganesh-import.pem" >> inventory
+      echo "${aws_instance.team42[0].private_ip} ansible_user=ec2-user ansible_ssh_private_key_file=~/.ssh/ganesh-import.pem" >> inventory
       echo "[nodes]" >> inventory
-      echo "${aws_instance.myawsserver[1].private_ip} ansible_user=ec2-user ansible_ssh_private_key_file=~/.ssh/ganesh-import.pem" >> inventory
-      echo "${aws_instance.myawsserver[2].private_ip} ansible_user=ec2-user ansible_ssh_private_key_file=~/.ssh/ganesh-import.pem" >> inventory
+      echo "${aws_instance.team42[1].private_ip} ansible_user=ec2-user ansible_ssh_private_key_file=~/.ssh/ganesh-import.pem" >> inventory
+      echo "${aws_instance.team42[2].private_ip} ansible_user=ec2-user ansible_ssh_private_key_file=~/.ssh/ganesh-import.pem" >> inventory
     EOT
   }
 }

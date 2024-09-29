8e60fdd8125826bca802b3607948e37da28e3bec
Ganesamoorthyselvaraj <s.ganesamoorthy@gmail.com>
2024-09-29 16:26:44 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -27,7 +27,8 @@ Name= var.elb-names[count.index]
       if [ ${count.index} -eq 1 ]; then
         echo "[nodes]" >> hosts.ini
         echo "${self.private_ip} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/id_rsa" >> hosts.ini
-      elif [ ${count.index} -gt 1 ]; then
+      fi
+      if [ ${count.index} -eq 2 ]; then
         echo "${self.private_ip} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/id_rsa" >> hosts.ini
       fi
     EOT

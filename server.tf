b015769db952a60c8129c80fd33ec4de09befd28
Ganesamoorthyselvaraj <s.ganesamoorthy@gmail.com>
2024-09-30 13:28:35 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -1,37 +1,34 @@
 variable "elb-names" {
-  type = list
-  default = ["ganesh-Kub-node2", "ganesh-Kub-node1","ganesh-Kub-Master"]
+  type    = list(string)
+  default = ["ganesh-Kub-Master", "ganesh-Kub-node1","ganesh-Kub-node2"]
 }
 
 variable "list" {
-  type = list
-  default = ["t2.medium","t2.medium","t2.large"]
+  type    = list(string)
+  default = ["t2.large", "t2.medium", "t2.medium"]
 }
-
 
 resource "aws_instance" "team42" {
-ami = "ami-0e86e20dae9224db8"
-key_name = "ganesh-import"
-count= 3
-   instance_type = var.list[count.index]
-tags= {
-Name= var.elb-names[count.index]
-}
+  count         = 3
+  ami           = "ami-0e86e20dae9224db8"
+  instance_type = var.list[count.index]
+  key_name      = "ganesh-import"
+
+  tags = {
+    Name = var.elb-names[count.index]
+  }
 
   provisioner "local-exec" {
-    command = <<EOT
+   command = <<EOT
       if [ ${count.index} -eq 0 ]; then
         echo "[master]" > hosts.ini
         echo "${self.private_ip} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/id_rsa" >> hosts.ini
-      fi
-      if [ ${count.index} -eq 1 ]; then
-        echo "${self.private_ip} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/id_rsa" >> hosts.ini
-      fi
-      if [ ${count.index} -eq 2 ]; then
+      elif [ ${count.index} -eq 1 ]; then
         echo "[nodes]" >> hosts.ini
+        echo "${self.private_ip} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/id_rsa" >> hosts.ini
+      else
         echo "${self.private_ip} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/id_rsa" >> hosts.ini
       fi
     EOT
-    
   }
 }

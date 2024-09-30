8c59628bbe5778f17d4d5e2e9fa141b50600c498
Ganesamoorthyselvaraj <s.ganesamoorthy@gmail.com>
2024-09-30 12:34:15 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -20,15 +20,15 @@ Name= var.elb-names[count.index]
 
   provisioner "local-exec" {
     command = <<EOT
-      if [ ${count.index} -eq 0 ]; then
+      if [ ${count.index} -eq 2 ]; then
         echo "[master]" >> hosts.ini
+        echo "${self.private_ip} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/id_rsa" >> hosts.ini
+      fi
+      if [ ${count.index} -eq 0 ]; then
         echo "${self.private_ip} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/id_rsa" >> hosts.ini
       fi
       if [ ${count.index} -eq 1 ]; then
         echo "[nodes]" >> hosts.ini
-        echo "${self.private_ip} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/id_rsa" >> hosts.ini
-      fi
-      if [ ${count.index} -eq 2 ]; then
         echo "${self.private_ip} ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/id_rsa" >> hosts.ini
       fi
     EOT
